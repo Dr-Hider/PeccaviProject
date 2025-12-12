36ada002928c0a672ca76f278057c88e6ef48bb4
Dr_Hider <drhider567@gmail.com>
2025-12-12 23:09:28 +1000

    Version 0.0.0.3

--- Assets/Scripts/Dialogue/Dialogue Manager.cs
+++ Assets/Scripts/Dialogue/Dialogue Manager.cs
@@ -6,26 +6,48 @@ using UnityEngine;
 
 public class DialogueManager : MonoBehaviour
 {
+    public string JsonPath { get; set; }
     public int StringNumber { get; set; }
     List<Dictionary<string, string>> data;
-    public TextAsset json;
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI dialogueText;
     public SpriteRenderer background;
 
+    public GameObject mainPanel;
+    public GameObject choicePanel;
+    public TextMeshProUGUI choice1Text;
+    public TextMeshProUGUI choice2Text;
+
     void Awake()
     {
-        data = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(json.text);
+        JsonPath = "Chapter 1/Introduction/Introduction";
+        UpdateJSON();
     }
     void Start()
     {
-        OnChangeString();
+        OnStringChange();
     }
 
-    public void OnChangeString()
+    void UpdateJSON()
+    {
+        data = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(Resources.Load<TextAsset>($"JSON/{JsonPath}").text);
+        StringNumber = 0;
+    }
+    void OnStringChange()
     {
         try
         {
+            if (StringNumber == data.Count - 1)
+            {
+                if (data[StringNumber].ContainsKey("choice"))
+                {
+                    mainPanel.SetActive(false);
+                    choicePanel.SetActive(true);
+                    choice1Text.text = data[StringNumber]["choice1string"];
+                    choice2Text.text = data[StringNumber]["choice2string"];
+                }
+                return;
+            }
             nameText.text = data[StringNumber]["name"];
             dialogueText.text = data[StringNumber]["text"];
             if (data[StringNumber]["image"] == "")
@@ -37,17 +59,33 @@ public class DialogueManager : MonoBehaviour
         {
             if (StringNumber < 0)
                 StringNumber = 0;
+            if (StringNumber > data.Count - 1)
+                StringNumber = data.Count - 1;
         }
 
     }
     public void Forward()
     {
         StringNumber++;
-        OnChangeString();
+        OnStringChange();
     }
     public void Back()
     {
         StringNumber--;
-        OnChangeString();
+        OnStringChange();
+    }
+    public void OnChoice(int choice)
+    {
+        switch (choice)
+        {
+            case 1:
+                JsonPath = data[StringNumber]["choice1path"];
+                break;
+            case 2:
+                JsonPath = data[StringNumber]["choice2path"];
+                break;
+        }
+        UpdateJSON();
+        OnStringChange();
     }
 }
